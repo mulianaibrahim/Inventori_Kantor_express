e8679705d4fc3701b64ddccd3634422925c77be1
lunarcwhite <rizkym058@gmail.com>
2023-06-26 20:00:55 +0700

    dynamic port

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const {server} = require('./server');
 
 const app = server();
 
-const PORT = process.env.APP_PORT;
-app.listen(PORT);
-console.log('application running on port', PORT);
+
+const PORT = 3000;
+const end = process.env.APP_PORT;
+if(Boolean(end)){
+    app.listen(end);
+    console.log('application running on port', end);
+}else{
+    app.listen(PORT);
+    console.log('application running on port', PORT);
+}
